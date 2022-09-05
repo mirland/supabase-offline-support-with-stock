baccd9463d6703cb10df69e6cb13e21950e028f6
mirland <mirland@xmartlabs.com>
2022-09-05 19:32:10 -0300

    Add project entity

---
+++ lib/model/project.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+
+class Project extends Equatable {
+  final int id;
+  final String name;
+  final String description;
+  final String url;
+  final String imageUrl;
+  final String language;
+
+  const Project({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.url,
+    required this.imageUrl,
+    required this.language,
+  });
+
+  factory Project.fromJson(Map<String, dynamic> json) => Project(
+        id: json['id'],
+        name: json['name'],
+        description: json['description'],
+        url: json['url'],
+        imageUrl: json['image_url'],
+        language: json['language'],
+      );
+
+  @override
+  bool? get stringify => true;
+
+  @override
+  List<Object?> get props => [id, name, description, url, imageUrl, language];
+}
